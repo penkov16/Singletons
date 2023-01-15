e0cd2995199646d8206e94e4b2a31d4a7b633258
Пеньков А.С <penkov-sasha@mail.ru>
2023-01-15 11:55:48 +0300

    Singletons

---
+++ app/src/main/java/ru/netology/nmedia/application/NMediaApplication.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.application
+
+import android.app.Application
+
+import ru.netology.nmedia.di.DependencyContainer
+
+class NMediaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DependencyContainer.initApp(this)
+    }
+}
---
+++ app/src/main/java/ru/netology/nmedia/viewmodel/AuthViewModel.kt
@@ -0,0 +1,18 @@
+package ru.netology.nmedia.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.Dispatchers
+import ru.netology.nmedia.auth.AppAuth
+import ru.netology.nmedia.auth.AuthState
+
+class AuthViewModel(
+private val appAuth: AppAuth,
+) : ViewModel() {
+    val data: LiveData<AuthState> = appAuth
+        .authStateFlow
+        .asLiveData(Dispatchers.Default)
+    val authenticated: Boolean
+        get() = appAuth.authStateFlow.value.id != 0L
+}
